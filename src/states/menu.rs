e78544bb7561b11a212f39e6445daec8ddebc372
JarredAllen <jarredallen73@gmail.com>
2020-07-27 16:22:14 -0600

    Add button interactivity

--- src/states/menu.rs
+++ src/states/menu.rs
@@ -1,5 +1,12 @@
-use crate::resources::{prefabs::UiPrefabRegistry, ResourceRegistry};
-use amethyst::{ecs::Entity, prelude::*, ui::UiFinder};
+use crate::{
+    resources::{prefabs::UiPrefabRegistry, ResourceRegistry},
+    states::Test,
+};
+use amethyst::{
+    ecs::Entity,
+    prelude::*,
+    ui::{UiEvent, UiEventType, UiFinder},
+};
 
 const MENU_ID: &str = "menu";
 
@@ -22,9 +29,43 @@ impl SimpleState for MenuState {
         data.data.update(&data.world);
         data.world.exec(|ui_finder: UiFinder<'_>| {
             self.start_button = ui_finder.find("start");
-            self.options_button = ui_finder.find("controls");
+            self.options_button = ui_finder.find("options");
             self.exit_button = ui_finder.find("exit");
         });
+    }
+
+    fn handle_event(&mut self, _data: StateData<'_, GameData<'_, '_>>, event: StateEvent,) -> SimpleTrans {
+        match event {
+            StateEvent::Ui(UiEvent { event_type: UiEventType::Click, target, }) => {
+                if self.start_button.map_or(false, |button| button == target) {
+                    Trans::Push(Box::new(Test::default()))
+                } else if self.options_button.map_or(false, |button| button == target) {
+                    unimplemented!("Options aren't implemented yet :(")
+                } else if self.exit_button.map_or(false, |button| button == target) {
+                    Trans::Quit
+                } else {
+                    Trans::None
+                }
+            }
+            _ => Trans::None,
+        }
+    }
+
+    fn on_pause(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
+        // TODO Hide the Ui elements
+    }
+
+    fn on_resume(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
+        // TODO Unhide the Ui elements
+    }
+
+    fn on_stop(&mut self, data: StateData<GameData>) {
+        let to_delete: Vec<Entity> = self.root_entity.into_iter().chain(self.start_button.into_iter()).chain(self.options_button.into_iter()).chain(self.exit_button.into_iter()).collect();
+        data.world.delete_entities(&to_delete).expect("Failed to remove menu elements");
+        self.root_entity = None;
+        self.start_button = None;
+        self.options_button = None;
+        self.exit_button = None;
     }
 
     fn update(&mut self, data: &mut StateData<GameData>) -> SimpleTrans {

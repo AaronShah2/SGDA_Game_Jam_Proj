2b4509dd9b96ddc0987c45841082a74413d09832
JarredAllen <jarredallen73@gmail.com>
2020-07-27 16:26:12 -0600

    Only show menu buttons when menu is visible

--- src/states/menu.rs
+++ src/states/menu.rs
@@ -51,12 +51,28 @@ impl SimpleState for MenuState {
         }
     }
 
-    fn on_pause(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
-        // TODO Hide the Ui elements
+    fn on_pause(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let to_delete: Vec<Entity> = self.root_entity.into_iter().chain(self.start_button.into_iter()).chain(self.options_button.into_iter()).chain(self.exit_button.into_iter()).collect();
+        data.world.delete_entities(&to_delete).expect("Failed to remove menu elements");
+        self.root_entity = None;
+        self.start_button = None;
+        self.options_button = None;
+        self.exit_button = None;
     }
 
-    fn on_resume(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
-        // TODO Unhide the Ui elements
+    fn on_resume(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let menu_prefab = data
+            .world
+            .read_resource::<UiPrefabRegistry>()
+            .find(data.world, MENU_ID)
+            .expect("Couldn't load menu prefab");
+        self.root_entity = Some(data.world.create_entity().with(menu_prefab).build());
+        data.data.update(&data.world);
+        data.world.exec(|ui_finder: UiFinder<'_>| {
+            self.start_button = ui_finder.find("start");
+            self.options_button = ui_finder.find("options");
+            self.exit_button = ui_finder.find("exit");
+        });
     }
 
     fn on_stop(&mut self, data: StateData<GameData>) {

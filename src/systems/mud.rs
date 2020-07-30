32e44eaa1edf31a5516c4ee02960069d42b76723
JarredAllen <jarredallen73@gmail.com>
2020-07-30 10:14:41 -0700

    Fix borrow checker and aliasing errors in mud system

--- src/systems/mud.rs
+++ src/systems/mud.rs
@@ -1,6 +1,6 @@
 // mud obj that slows player and enemy down
 use crate::{
-    components::{Mud, Player, Enemy},
+    components::{Enemy, Mud, Player},
     resources::Paused,
 };
 use amethyst::{
@@ -14,39 +14,33 @@ const COLLISION_RADIUS: f32 = 510.0;
 pub struct MudSystem;
 
 impl<'s> System<'s> for MudSystem {
+    #[allow(clippy::type_complexity)]
     type SystemData = (
-        WriteStorage<'s, Transform>,
+        ReadStorage<'s, Transform>,
         WriteStorage<'s, Player>,
         ReadStorage<'s, Enemy>,
-        ReadStorage<'s, Mud>, 
+        ReadStorage<'s, Mud>,
         Read<'s, Paused>,
     );
 
     //TODO: Fix collision hitbox, slow down enemy
-    fn run(&mut self, (transforms, players, enemies, mud, paused): Self::SystemData) {
-        // Pauses game 
+    fn run(&mut self, (transforms, mut players, enemies, mud, paused): Self::SystemData) {
+        // Pauses game
         if *paused == Paused::Paused {
             return;
         }
         // checks if player collides with mud
-        for ((player, player_transform), (_, mud_transform)) in
-            (&players, &transforms).join().flat_map(|p| {
-                (&mud, &transforms)
-                    .join()
-                    .map(|m| (p, m))
-                    .collect::<Vec<_>>()
-            })
-        {
-            if (player_transform.translation() - mud_transform.translation()).norm()
-                <= COLLISION_RADIUS
-            {
-                player.slow_down();
-            }
-            else {
-                player.normal_speed();
+        for (player, player_transform) in (&mut players, &transforms).join() {
+            for (_, mud_transform) in (&mud, &transforms).join() {
+                if (player_transform.translation() - mud_transform.translation()).norm()
+                    <= COLLISION_RADIUS
+                {
+                    player.slow_down();
+                } else {
+                    player.normal_speed();
+                }
             }
         }
-
         // checks if enemy collides with mud
         for ((_, enemy_transform), (_, mud_transform)) in
             (&enemies, &transforms).join().flat_map(|e| {
